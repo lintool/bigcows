50d5f36c8f065899cac92b92d745011880f38bed
lintool <jimmylin@umd.edu>
2014-02-15 23:20:38 -0500

    Added a few IR researchers, simple table view in HTML.

--- scrape.js
+++ scrape.js
@@ -2,19 +2,32 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
+var async = require('async');
 
-people = {
+var people = {
+  'James Allan': 'http://scholar.google.com/citations?user=-bLGeg0AAAAJ&hl=en',
+  'Eugene Agichtein': 'http://scholar.google.com/citations?user=3BX3vWcAAAAJ&hl=en',
+  'Charles L. A. Clarke': 'http://scholar.google.com/citations?user=TkVleDIAAAAJ&hl=en',
+  'W. Bruce Croft': 'http://scholar.google.com/citations?user=ArV74ZMAAAAJ&hl=en',
+  'Susan Dumais': 'http://scholar.google.com/citations?user=x8dED5cAAAAJ&hl=en',
+  'Marti A. Hearst': 'http://scholar.google.com/citations?user=Yy6xbCYAAAAJ&hl=en',
+  'Joemon M Jose': 'http://scholar.google.com/citations?user=ERvFJGkAAAAJ&hl=en',
+  'Mounia Lalmas': 'http://scholar.google.com/citations?user=wAr9G5sAAAAJ&hl=en',
   'Jimmy Lin' : 'http://scholar.google.com/citations?user=0EWw1z8AAAAJ&hl=en',
+  'Douglas W. Oard' : 'http://scholar.google.com/citations?user=3ctKkysAAAAJ&hl=en',
+  'Cornelis van Rijsbergen': 'http://scholar.google.com/citations?user=NnFpD_sAAAAJ&hl=en',
+  'Jaime Teevan': 'http://scholar.google.com/citations?user=F5Ik84MAAAAJ&hl=en',
+  'Arjen P. de Vries': 'http://scholar.google.com/citations?user=iH9TVHQAAAAJ&hl=en',
+  'Ryen W. White': 'http://scholar.google.com/citations?user=U69fiZMAAAAJ&hl=en',
+  'ChengXiang Zhai': 'http://scholar.google.com/citations?user=YU-baPIAAAAJ&hl=en',
 }
 
-var data = [];
-
-for (person in people) {
+var process = function(person, doneCallback) {
   var url = people[person];
+  var data = {};
 
   request(url, (function(person) { return function(err, resp, body) {
     $ = cheerio.load(body);
-    console.log(person);
 
     var raw = $('#stats tr');
     var stats = {
@@ -23,8 +36,13 @@ for (person in people) {
       'i10index' : [raw[3].children[1].children[0].data, raw[3].children[2].children[0].data]
     };
 
-    console.log(stats);
+    data = { 'name' : person, 'stats' : stats};
 
-    data.push({ 'name' : person, 'stats' : stats});
+    doneCallback(null, data);
   }})(person));
-}
+};
+
+// http://javascriptplayground.com/blog/2013/06/think-async/
+async.map(Object.keys(people), process, function (err, results) {
+  console.log('var data = ' + JSON.stringify(results, null, 2) + ';');
+});
