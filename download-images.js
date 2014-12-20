8791c87bee8ff3fbc9f2839ba745e58ef385605b
lintool <jimmylin@umd.edu>
2014-12-19 20:22:25 -0500

    Added image downloader, scrape of IR researchers.

---
+++ download-images.js
@@ -0,0 +1,40 @@
+// From http://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
+var fs = require('fs');
+
+filedata = fs.readFileSync(process.argv[2], 'utf8');
+eval(filedata);
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    //console.log('content-type:', res.headers['content-type']);
+    //console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+/*
+download('http://scholar.google.com/citations?view_op=view_photo&user=0EWw1z8AAAAJ&citpid=1', 'images/jimmy.png', function(){
+  console.log('done');
+});
+*/
+
+var scrapeImage = function(entry, doneCallback) {
+  var url = entry.photo.replace('&amp;', '&');
+  var regex = /user=([^&]+)/;
+  var match = regex.exec(entry.url);
+  var user = match[1];
+
+  download(url, 'images/' + user + '.png', function(){
+    console.log('Downloading image for ' + entry.name + ': ' + url);
+    doneCallback(null, entry);
+  });
+
+};
+
+async.mapSeries(data, scrapeImage, function (err, results) {
+  console.log('Done!');
+});
