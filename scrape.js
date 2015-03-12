84f03064ea1f39d6d2369c38b227c4c9f10a89e5
JJ Merelo <jjmerelo@gmail.com>
2015-03-12 07:48:17 +0100

    Adds package.json for easy installation

--- scrape.js
+++ scrape.js
@@ -6,50 +6,50 @@ var async = require('async');
 var people = require(process.argv[2]);
 
 var scrapeEntry = function(person, doneCallback) {
-  var url = people[person];
-  var data = {};
+    var url = people[person];
+    var data = {};
 
-  // properly set the encoding, or we'll mangle accented characters:
-  // http://stackoverflow.com/questions/8332500/module-request-how-to-properly-retrieve-accented-characters-%EF%BF%BD-%EF%BF%BD-%EF%BF%BD
-  request({ encoding: 'binary', method: "GET", uri: url}, function(err, resp, body) {
-    $ = cheerio.load(new String(body));
+    // properly set the encoding, or we'll mangle accented characters:
+    // http://stackoverflow.com/questions/8332500/module-request-how-to-properly-retrieve-accented-characters-%EF%BF%BD-%EF%BF%BD-%EF%BF%BD
+    request({ encoding: 'binary', method: "GET", uri: url}, function(err, resp, body) {
 
-    try {
-      var photo = $('#gsc_prf_pup')[0].attribs.src.replace(/&amp;/g, '&');
-      var affiliation = $('.gsc_prf_il', '#gsc_prf_i')[0].children[0].data;
-
-      var keywords_root = $('.gsc_prf_ila');
-      var keywords = [];
-
-      for (var i=0; i<keywords_root.length; i++) {
-        keywords.push(keywords_root[i].children[0].data);
-      }
-
-      var rawStats = $('#gsc_rsb_st');
-      var stats = {
-        'citations' : [ rawStats[0].children[1].children[1].children[0].data,
-                        rawStats[0].children[1].children[2].children[0].data],
-        'hindex' : [ rawStats[0].children[2].children[1].children[0].data,
-                     rawStats[0].children[2].children[2].children[0].data],
-        'i10index' : [ rawStats[0].children[3].children[1].children[0].data,
-                       rawStats[0].children[3].children[2].children[0].data]
-      };
-
-      data = {
-        'name' : person,
-        'photo' : 'http://scholar.google.com' + photo,
-        'keywords' : keywords,
-        'affiliation' : affiliation,
-        'url': url,
-        'stats' : stats
-      };
-
-    } catch (ex) {
-      throw new Error(person);
-    }
-
-    doneCallback(null, data);
-  });
+	$ = cheerio.load(new String(body));
+	try {
+	    var photo = $('#gsc_prf_pup')[0].attribs.src.replace(/&amp;/g, '&');
+	    var affiliation = $('.gsc_prf_il', '#gsc_prf_i')[0].children[0].data;
+	    
+	    var keywords_root = $('.gsc_prf_ila');
+	    var keywords = [];
+	    for (var i=0; i<keywords_root.length; i++) {
+		keywords.push(keywords_root[i].children[0].data);
+	    }
+	
+	    var rawStats = $('#gsc_rsb_st');
+	    console.log( rawStats );
+	    var stats = {
+		'citations' : [ rawStats[0].children[1].children[1].children[0].data,
+				rawStats[0].children[1].children[2].children[0].data],
+		'hindex' : [ rawStats[0].children[2].children[1].children[0].data,
+			     rawStats[0].children[2].children[2].children[0].data],
+		'i10index' : [ rawStats[0].children[3].children[1].children[0].data,
+			       rawStats[0].children[3].children[2].children[0].data]
+	    };
+	    
+	    data = {
+		'name' : person,
+		'photo' : 'http://scholar.google.com' + photo,
+		'keywords' : keywords,
+		'affiliation' : affiliation,
+		'url': url,
+		'stats' : stats
+	    };
+	    
+	} catch (ex) {
+	    throw new Error(person);
+	}
+	
+	doneCallback(null, data);
+    });
 };
 
 var scrapeYear = function(entry, doneCallback) {
