91880d7c57645c2ac88d27765b1d9885e18f31d0
lintool <jimmylin@uwaterloo.ca>
2017-10-21 11:03:23 -0400

    Updated to new Google Scholar layout... seems to work but code still rough.

--- scrape.js
+++ scrape.js
@@ -18,36 +18,65 @@ var scrapeEntry = function(person, doneCallback) {
       // We're output to stdout, so log to stderr
       console.error("Scraping " + person + "...");
 
-      var photo = $('#gsc_prf_pup')[0].attribs.src.replace(/&amp;/g, '&');
+      //console.error($('<img id="gsc_prf_pup-img">'));
+      //console.error($('#gsc_prf_i')[0].children[1]);
+      //console.error($('#gsc_prf_i')[0].children[1].children[0].text());
+      //console.error($('.gsc_prf_il', '#gsc_prf_i').first().text());
+
+      var photo = $('#gsc_prf_pup-img')[0].attribs.src;
       var affiliation = $('.gsc_prf_il', '#gsc_prf_i').first().text();
 
-      var keywords_root = $('.gsc_prf_ila');
+      var keywords_root = $('#gsc_prf_int')[0].children;
+      //console.error(keywords_root);
       var keywords = [];
 
       for (var i=0; i<keywords_root.length; i++) {
         keywords.push(keywords_root[i].children[0].data);
+      //  keywords.push(keywords_root[i].children[0].data);
       }
 
       var rawStats = $('#gsc_rsb_st');
+      //console.error(rawStats);
+/*
+      console.error(rawStats[0].children[1].children[0].children[1].children[0].data);
+      console.error(rawStats[0].children[1].children[0].children[2].children[0].data);
+
+      console.error(rawStats[0].children[1].children[1].children[1].children[0].data);
+      console.error(rawStats[0].children[1].children[1].children[2].children[0].data);
+
+      console.error(rawStats[0].children[1].children[2].children[1].children[0].data);
+      console.error(rawStats[0].children[1].children[2].children[2].children[0].data);
+*/
+      //console.error('0.' + rawStats[0].data);
+      //console.error('1.' + rawStats[0].children[1].data);
+
       var stats = {
-        'citations' : [ rawStats[0].children[1].children[1].children[0].data,
-                        rawStats[0].children[1].children[2].children[0].data],
-        'hindex' : [ rawStats[0].children[2].children[1].children[0].data,
-                     rawStats[0].children[2].children[2].children[0].data],
-        'i10index' : [ rawStats[0].children[3].children[1].children[0].data,
-                       rawStats[0].children[3].children[2].children[0].data]
+        'citations' : [ rawStats[0].children[1].children[0].children[1].children[0].data,
+                        rawStats[0].children[1].children[0].children[2].children[0].data ],
+        'hindex'    : [ rawStats[0].children[1].children[1].children[1].children[0].data,
+                        rawStats[0].children[1].children[1].children[2].children[0].data ],
+        'i10index'  : [ rawStats[0].children[1].children[2].children[1].children[0].data,
+                        rawStats[0].children[1].children[2].children[2].children[0].data ]
       };
+
+//console.log(stats);
+
+      var rawYear = $('.gsc_md_hist_b');
+      console.error();
+      //console.error(rawYear['_root'][0]);
 
       data = {
         'name' : person,
-        'photo' : 'http://scholar.google.com' + photo,
-        'keywords' : keywords,
-        'affiliation' : affiliation,
         'url': url,
-        'stats' : stats
+        'photo' : 'http://scholar.google.com' + photo,
+        'affiliation' : affiliation,
+        'keywords' : keywords,
+        'stats' : stats,
+        'year' : rawYear[0].children[0].children[0].data
       };
 
     } catch (ex) {
+      console.error(ex);
       throw new Error(person);
     }
 
@@ -58,29 +87,9 @@ var scrapeEntry = function(person, doneCallback) {
   });
 };
 
-var scrapeYear = function(entry, doneCallback) {
-  var histUrl = entry['url'] + "&view_op=citations_histogram";
-
-  request(histUrl, function(err, resp, body) {
-    $ = cheerio.load(body);
-
-    try {
-      year = $('.gsc_g_t')[0].children[0].data;
-    } catch(ex) {
-      throw new Error(entry['url']);
-    }
-
-    entry['year'] = year;
-
-    doneCallback(null, entry);
-  });
-};
-
 // http://javascriptplayground.com/blog/2013/06/think-async/
 async.mapSeries(Object.keys(people), scrapeEntry, function (err, results) {
-  async.mapSeries(results, scrapeYear, function (err, results) {
-    var date = new Date();
-    console.log('var date = "' + date + '"');
-    console.log('var data = ' + JSON.stringify(results, null, 2) + ';');
-  });
+  var date = new Date();
+  console.log('var date = "' + date + '"');
+  console.log('var data = ' + JSON.stringify(results, null, 2) + ';');
 });
